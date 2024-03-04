f2fdd136af61e73e40c1b09cc0e4acca65a5a2d3
sushantdigitalat <sushant.khairnar@digitalat.in>
2024-03-04 16:57:47 +0530

    monday

---
+++ assets/js/scripts.js
@@ -0,0 +1,32 @@
+function active_course() {
+    if ($(".active_course").length) {
+      $(".active_course").owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 3,
+        nav: true,
+        autoplay: 5000,
+        smartSpeed: 1500,
+        dots: true,
+        responsiveClass: true,
+        thumbs: true,
+        thumbsPrerendered: true,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        responsive: {
+          0: {
+            items: 1,
+            margin: 0
+          },
+          991: {
+            items: 2,
+            margin: 30
+          },
+          1200: {
+            items: 3,
+            margin: 30
+          }
+        }
+      });
+    }
+  }
+  active_course(); 
