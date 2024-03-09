fc729ef98b52fe432482931506da2975a70834de
sushantdigitalat <sushant.khairnar@digitalat.in>
2024-03-09 13:06:17 +0530

    saturday

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,7 @@ function active_course() {
         items: 3,
         nav: true,
         autoplay: 5000,
-        smartSpeed: 1500,
+        smartSpeed: 2500,
         dots: true,
         responsiveClass: true,
         thumbs: true,
